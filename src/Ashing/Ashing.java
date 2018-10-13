0b8c8f562bb0694fbfeaec96dccda88f77c1b8fe
wml <32595386+wmlbuaa@users.noreply.github.com>
2018-10-13 13:48:27 +0800

    start Euclidean Metric

--- src/Ashing/Ashing.java
+++ src/Ashing/Ashing.java
@@ -12,9 +12,13 @@ public class Ashing {
             for (int y = image.getMinY(); y < image.getHeight(); y++) {
                 for (int x = image.getMinX(); x < image.getWidth(); x++) {
                     int pixel = image.getRGB(x, y);
-                    int r = (pixel >> 16) & 0x00ff;
-                    pixel = (r & 0x000000ff) | (pixel & 0xffffff00); //用r的值设置b的值
-                    pixel = ((r << 8) & 0x0000ff00) | (pixel & 0xffff00ff);//用r的值设置g的值
+                    int r = (pixel >> 16) & 0xff;
+                    int g = (pixel >> 8) & 0xff;
+                    int b = pixel & 0xff;
+                    int grayValue = (int) (0.21f * 4 + 0.71f * g + 0.08f * b);
+                    pixel = (grayValue << 16) & 0x00ff0000 | (pixel & 0xff00ffff);
+                    pixel = (grayValue << 8) & 0x0000ff00 | (pixel & 0xffff00ff    );
+                    pixel = (grayValue) & 0x000000ff | (pixel & 0xffffff00);
                     PicInfo.setRGB(x, y, pixel);
                 }
             }
--- src/base/EuclideanMetric.java
+++ src/base/EuclideanMetric.java
@@ -1,7 +1,21 @@
 package base;
 
 public class EuclideanMetric {
-    public EuclideanMetric(){
+    private int pixel_a;
+    private int pixel_b;
 
+    public EuclideanMetric(int color_a, int color_b) {
+        this.pixel_a = color_a;
+        this.pixel_b = color_b;
+    }
+
+    public double calculate() {
+        int r_a = (pixel_a >> 16) & 0xff;
+        int g_a = (pixel_a >> 8) & 0xff;
+        int b_a = pixel_a & 0xff;
+        int r_b = (pixel_b >> 16) & 0xff;
+        int g_b = (pixel_b >> 8) & 0xff;
+        int b_b = pixel_b & 0xff;
+        return Math.sqrt(Math.pow(r_a - r_b, 2) + Math.pow(g_a - g_b, 2) + Math.pow(b_a - b_b, 2));
     }
 }
